fa767ada141eece862a5343f518b556d0f29a3ab
Carsten Stein <carsten.stein@cast-it.de>
2020-04-17 18:57:51 +0200

    Add react utils

--- packages/rocklab-react-utils/index.js
+++
@@ -1,2 +0,0 @@
-export { default as getDisplayName } from './src/helper/getDisplayName';
-export { default as useToggle } from './src/hooks/toggle';
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import react from './react';
+
+export default {
+  'react-utils': react,
+};
