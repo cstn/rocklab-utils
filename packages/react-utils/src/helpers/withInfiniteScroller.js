d96f754ef7e2ab8a8b8770ebf80a9a03d2bd5540
Carsten Stein <carsten.stein@cast-it.de>
2020-05-23 15:24:36 +0200

    feature/helper (#8)
    * Add scroller helper
    * Add previous hook

---
+++ packages/react-utils/src/helpers/withInfiniteScroller.js
@@ -0,0 +1,51 @@
+/**
+ * @fileOverview infinite scroller
+ */
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import getDisplayName from '../utils/getDisplayName';
+
+function withInfiniteScroller(WrappedComponent, options = { offset: 0 }) {
+  class WithInfiniteScroller extends Component {
+    componentDidMount() {
+      window.addEventListener('scroll', this.onScroll, false);
+    }
+
+    componentWillUnmount() {
+      window.removeEventListener('scroll', this.onScroll, false);
+    }
+
+    onScroll = () => {
+      const { hasMore, onMore } = this.props;
+      const { offset } = options;
+
+      const { scrollHeight } = document.body;
+      const height = window.scrollY + window.innerHeight;
+      const isAtBottom = height >= scrollHeight - offset;
+
+      if (isAtBottom && hasMore) {
+        onMore();
+      }
+    };
+
+    render() {
+      return <WrappedComponent {...this.props} />;
+    }
+  }
+
+  WithInfiniteScroller.displayName = `withInfiniteScroller(${getDisplayName(WrappedComponent)})`;
+  WithInfiniteScroller.propTypes = {
+    isLoading: PropTypes.bool,
+    hasMore: PropTypes.bool,
+    onMore: PropTypes.func.isRequired,
+  };
+
+  WithInfiniteScroller.defaultProps = {
+    hasMore: true,
+  };
+
+  return WithInfiniteScroller;
+}
+
+export default withInfiniteScroller;
