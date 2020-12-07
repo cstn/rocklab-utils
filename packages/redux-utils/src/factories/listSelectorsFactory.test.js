7c86b7113120804fb5b69a3feeb03487ed37cbef
Carsten Stein <carsten.stein@cast-it.de>
2020-12-07 12:46:52 +0100

    Add selectors

--- packages/redux-utils/src/factories/listSelectorsFactory.test.js
+++ packages/redux-utils/src/factories/listSelectorsFactory.test.js
@@ -13,17 +13,54 @@ const mockState = {
       list: mockList,
       status: mockStatus,
       error: mockError,
+      total: 99,
+      count: 3,
+      limit: 100,
+      offset: 0,
     },
   },
 };
 
 describe('listSelectorsFactory', () => {
-  const { selectList, selectListSize, selectError, selectStatus } = createSelectors('test/a');
+  const {
+    selectList,
+    selectListSize,
+    selectError,
+    selectStatus,
+    selectLimit,
+    selectOffset,
+    selectCount,
+    selectTotal,
+  } = createSelectors('test/a');
 
   it('should select the list', () => {
     const selected = selectList(mockState);
 
     expect(selected).toEqual(mockList);
+  });
+
+  it('should select the limit', () => {
+    const selected = selectLimit(mockState);
+
+    expect(selected).toEqual(100);
+  });
+
+  it('should select the offset', () => {
+    const selected = selectOffset(mockState);
+
+    expect(selected).toEqual(0);
+  });
+
+  it('should select the total count', () => {
+    const selected = selectTotal(mockState);
+
+    expect(selected).toEqual(99);
+  });
+
+  it('should select the item count', () => {
+    const selected = selectCount(mockState);
+
+    expect(selected).toEqual(3);
   });
 
   it('should select the list size', () => {
