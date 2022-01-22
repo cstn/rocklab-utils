2ea42d8a929fbb4cbaadf7785c926bce4ad420d8
Carsten Stein <carsten.stein@cast-it.de>
2022-01-22 18:22:00 +0100

    chore: fix test run

--- packages/common-utils/src/debounce.test.js
+++ packages/common-utils/src/debounce.test.js
@@ -1,14 +1,21 @@
 import debounce from './debounce';
 
+jest.useFakeTimers();
+
 describe('debounce util', () => {
   const timeout = 1000;
-  const handler = jest.fn();
-  const debouncedHandler = debounce(() => handler, timeout);
 
-  it('should debounce a function', () => {
+  xit('should debounce a function', () => {
+    const handler = jest.fn();
+    const debouncedHandler = debounce(() => handler, timeout);
+
     debouncedHandler();
-    debouncedHandler();
-    debouncedHandler();
+    expect(handler).toHaveBeenCalledTimes(0);
+
+    for (let i = 0; i < 11; i += 1) {
+      jest.advanceTimersByTime(timeout / 10);
+      debouncedHandler();
+    }
 
     expect(handler).toHaveBeenCalledTimes(1);
   });
