d85b6b1704bf580823a3308b0dff858e099fa29b
Carsten Stein <carsten.stein@cast-it.de>
2022-02-27 12:10:02 +0100

    feat(react-utils): add exports

--- packages/react-utils/src/index.ts
+++ packages/react-utils/src/index.ts
@@ -1,4 +1,3 @@
-export { default as useInterval } from './hooks/useInterval';
-export { default as usePrevious } from './hooks/usePrevious';
-export { default as useToggle } from './hooks/useToggle';
-export { User, UserProfile, CurrentUserProvider, useCurrentUser, withCurrentUser } from './currentUser';
+export * from './hooks';
+export * from './helpers';
+export * from './currentUser';
