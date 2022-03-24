ae5bf7c24f42bf1e0c35c43305ea5d5c980ab61c
Carsten Stein <carsten.stein@cast-it.de>
2022-03-24 19:54:00 +0100

    fix(redux-utils): fix selector export

--- packages/redux-utils/src/auth/session/index.ts
+++ packages/redux-utils/src/auth/session/index.ts
@@ -1,6 +1,7 @@
 export { default as sessionSlice } from './sessionSlice';
 export {
   selectAccessToken,
+  selectRefreshToken,
   selectUser as selectCurrentUser,
   selectUserProfile as selectCurrentUserProfile,
   selectStatus as selectSessionStatus,
