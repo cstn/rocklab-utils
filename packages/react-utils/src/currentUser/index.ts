b7a986e9b280f989272ecbf0fe43688b8a0dc779
Carsten Stein <carsten.stein@cast-it.de>
2022-03-26 18:58:42 +0100

    feat(react-utils): convert model to types

--- packages/react-utils/src/currentUser/index.ts
+++ packages/react-utils/src/currentUser/index.ts
@@ -1,5 +1,6 @@
 export { default as User } from './domain/models/User';
 export { default as UserProfile } from './domain/models/UserProfile';
+export { initials, fullName } from './domain/services/UserService';
 export { default as CurrentUserProvider } from './context/CurrentUserProvider';
 export { default as withCurrentUser } from './helpers/withCurrentUser';
 export { default as useCurrentUser } from './hooks/useCurrentUser';
